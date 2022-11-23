326ccf18119f2345f4ca6560f2a028ca22a7a909
hassanad <hassanad94@gmail.com>
2022-11-23 20:40:06 +0100

    deployed version

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const options = {
+  dotfiles: "ignore",
+  etag: true,
+  extensions: ["htm", "html"],
+  index: false,
+  maxAge: "7d",
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set("x-timestamp", Date.now());
+  },
+};
+
+// app.use(serveStatic(path.join(__dirname, "public")));
+
+app.use(express.static("public", options));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/main.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
